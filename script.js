d8ea17afbd91250f331dfd5467057b5e773877dd
Your Name <you@example.com>
2024-06-06 23:30:50 +0530

    "Updated index.html, script.js, and style.css files with new code, including changes to HTML structure, CSS styles, and JavaScript functions."

--- script.js
+++ script.js
@@ -30,3 +30,12 @@ function hideElement() {
       easing: "easeOutExpo",
       delay: 1000
     });
+
+    function openNav() {
+      document.getElementById("mySidenav").style.width = "100%";
+    }
+    
+    function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+    }
+    
